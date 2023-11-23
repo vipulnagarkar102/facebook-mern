5a56e7b8cad0d449c233adf02bb7dc6a048fc2aa
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2023-11-23 22:00:09 +0530

    start

---
+++ facebook/backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const { readdirSync } = require("fs");
+const app = express();
+app.use(cors());
+
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+app.listen(8000, () => {
+  console.log("server is lestining...");
+});
