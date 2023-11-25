1db15b474ae10b59635aedbfac518364d91cc974
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2023-11-25 11:51:59 +0530

    project setup

--- facebook/backend/controllers/user.js
+++ facebook/backend/controllers/user.js
@@ -1,3 +1,5 @@
 exports.home = (req, res) => {
-  res.send("welcome from user home");
+  res.status(200).json({
+    message:"vipul nsg",
+  })
 };
--- facebook/backend/server.js
+++ facebook/backend/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const { readdirSync } = require("fs");
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 //routes
