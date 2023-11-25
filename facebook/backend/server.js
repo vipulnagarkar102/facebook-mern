892e841d9bbefa3eba2ead8a8caa588493e72b76
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2023-11-25 11:01:49 +0530

    connection with mingodb

--- facebook/backend/server.js
+++ facebook/backend/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const { readdirSync } = require("fs");
 const app = express();
 app.use(cors());
 
+//routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-app.listen(8000, () => {
-  console.log("server is lestining...");
+//databse
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("error connecting to databse", err));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}....`);
 });
