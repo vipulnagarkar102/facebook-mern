46ed8305bc503690ba984ee036e7020a816c3185
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2023-11-28 18:04:24 +0530

    Register and send email, activate email

--- facebook/backend/controllers/user.js
+++ facebook/backend/controllers/user.js
@@ -1,4 +1,3 @@
-const { generateToken } = require("../helpers/tokens");
 const {
   validateEmail,
   validateLength,
@@ -6,6 +5,9 @@ const {
 } = require("../helpers/validation");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const { generateToken } = require("../helpers/tokens");
+const jwt=require("jsonwebtoken");
+const { sendVerificationEmail } = require("../helpers/mailer");
 exports.register = async (req, res) => {
   try {
     const {
@@ -64,15 +66,37 @@ exports.register = async (req, res) => {
       bDay,
       gender,
     }).save();
-    const emailverificationToken = generateToken(
-      {
-        id: user._id.toString(),
-      },
+    const emailVerificationToken = generateToken(
+      { id: user._id.toString() },
       "30m"
     );
-    console.log(emailverificationToken);
-    res.json(user);
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    sendVerificationEmail(user.email, user.first_name, url);
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Register Success ! please activate your email to start",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+exports.activateAccount =async (req, res) => {
+  const { token } = req.body;
+  const user=jwt.verify(token,process.env.TOKEN_SECRET);
+  const check=await User.findById(user.id);
+  if(check.verified==true){
+    return res.status(400).json({message:"This email already activated"})
+  }else{
+    await User.findByIdAndUpdate(user.id,{verified:true});
+    return res.status(200).json({message:"Account has benn activated successfully"});
+  }
+
 };
