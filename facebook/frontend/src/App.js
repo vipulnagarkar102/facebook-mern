d2e758782e13469aa651a041e89708051b3886a0
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2023-11-29 17:40:50 +0530

    router dom

--- facebook/frontend/src/App.js
+++ facebook/frontend/src/App.js
@@ -1,10 +1,15 @@
+import {Routes,Route} from "react-router-dom";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
 function App() {
-  const get = async () => {
-    const res = await fetch("http://localhost:8000");
-    console.log(res);
-  };
-  get();
-  return <div>welcome to frontend</div>;
+  return<div>
+  <Routes>
+  <Route path="/login" element={<Login/>}/>
+  <Route path="/profile" element={<Profile/>}/>
+  </Routes>
+  
+  </div>;
+  
 }
 
 export default App;
