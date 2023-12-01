0aee21a54c8f8c7e1d52fe7d14e62452563b7ccd
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2023-12-01 12:37:38 +0530

    Redux store

--- facebook/frontend/src/index.js
+++ facebook/frontend/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { BrowserRouter } from "react-router-dom";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
