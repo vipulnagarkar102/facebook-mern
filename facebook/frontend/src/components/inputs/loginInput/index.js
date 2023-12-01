fa1275969ba37b0cf3c63f81d5f2076e8889e98e
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2023-12-01 17:23:06 +0530

    login & register-1

---
+++ facebook/frontend/src/components/inputs/loginInput/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+import { useField } from "formik";
+
+const LoginInput = ({ placeholder, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className="input_wrap">
+      <input
+        type={field.type}
+        name={field.name}
+        placeholder={placeholder}
+        {...field}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default LoginInput;
