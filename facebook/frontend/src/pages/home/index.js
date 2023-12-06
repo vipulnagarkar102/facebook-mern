a4b3cd030012960d975d7faa14f3743b482a2267
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2023-12-06 16:59:10 +0530

    Header onclick outside

--- facebook/frontend/src/pages/home/index.js
+++ facebook/frontend/src/pages/home/index.js
@@ -1,5 +1,17 @@
+import { useRef, useState } from "react";
 import Header from "../../components/header";
+import useClickOutside from "../../helpers/clickOutside";
 
 export default function Home() {
-  return <Header/>;
+  const [visible, setVisible] = useState(true);
+  const el = useRef(null);
+  useClickOutside(el, () => {
+    setVisible(false);
+  });
+  return (
+    <div>
+      <Header />
+      {visible && <div className="card" ref={el}></div>};
+    </div>
+  );
 }
